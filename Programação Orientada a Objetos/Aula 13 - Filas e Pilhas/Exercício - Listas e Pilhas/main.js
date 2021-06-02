3d22b3b529737df25c6dd0c647ba8740e154c011
Thiago <zazirskas@hotmail.com>
2021-06-02 20:57:38 -0300

    Realizar Venda

--- Programação Orientada a Objetos/Aula 13 - Filas e Pilhas/Exercício - Listas e Pilhas/main.js
+++ Programação Orientada a Objetos/Aula 13 - Filas e Pilhas/Exercício - Listas e Pilhas/main.js
@@ -12,10 +12,27 @@ let opcao, opcao2, nome, quantidade, preco, fornecedor;
 
 console.log('Bem vindo ao sistema de estoque!');
 
+mostrarItensEstoque = () => {
+  let estoque = lerEstoque();
+
+  console.log('-----Itens de Mercearia-----');
+  for (item of estoque.EstoqueMercearia) {
+    console.log(`Nome: ${item.Nome} Quantidade: ${item.Quantidade} Preço: ${item.Preço} Fabricante: ${item.Fabricante}`);
+  }
+  console.log('\n')
+  console.log('-----Itens de Horti-Fruti-----');
+  for (item of estoque.EstoqueHortiFruti) {
+    console.log(`Nome: ${item.Nome} Quantidade: ${item.Quantidade} Preço: ${item.Preço} Produtor: ${item.Produtor}`);
+  }
+  console.log('\n');
+}
+
 while (opcao != 0) {
   console.log('1 - Adicione novo produto ao estoque');
   console.log('2 - Remova um produto do estoque');
   console.log('3 - Exportar estoques');
+  console.log('4 - Realizar venda');
+  console.log('5 - Mostrar produtos em estoque');
   console.log('0 - Sair');
 
   opcao = input.question('Insira a opção desejada: ')
@@ -79,8 +96,9 @@ while (opcao != 0) {
     
     console.log('--------------');
     let estoques = lerEstoque();
-    const EstoqueMercearia = []
-    const EstoqueHortiFruti = []
+
+    const EstoqueMercearia = estoques.EstoqueMercearia;
+    const EstoqueHortiFruti = estoques.EstoqueHortiFruti;
     
     for (item of estoqueMercearia.listaProdutos) {
       const novoItem = {Nome: item.nome, Quantidade: item.quantidade, Preço: parseInt(item.preco).toFixed(2), Fabricante: item.fabricante}
@@ -94,7 +112,77 @@ while (opcao != 0) {
     
     exportarEstoque({EstoqueMercearia: EstoqueMercearia, EstoqueHortiFruti: EstoqueHortiFruti});
     console.log('Estoques exportados com sucesso!');
+
+  } else if (opcao == 4) {
+    console.log('-----Estoque disponível para venda-----\n');
+
+    let estoque = lerEstoque();
+
+    const EstoqueMercearia = estoque.EstoqueMercearia;
+    const EstoqueHortiFruti = estoque.EstoqueHortiFruti;
+
+    mostrarItensEstoque();
+
+    nome = input.question('Insira o nome do item que deseja comprar: ');
+    if (!nome) {
+      console.log('Este item não está disponível!');
+    }
+
+    let indiceItemMercearia = EstoqueMercearia.findIndex((item) => item.Nome.toUpperCase() === nome.toUpperCase());
+    let indiceItemHortiFruti = EstoqueHortiFruti.findIndex((item) => item.Nome.toUpperCase() === nome.toUpperCase());
+    
+    if (indiceItemMercearia === -1 && indiceItemHortiFruti === -1) {
+      console.log('Este item não existe na lista de itens disponíveis!');
+      nome = input.question('Insira novamente o nome do item que deseja comprar: ');
+    }
+
+    indiceItemMercearia = EstoqueMercearia.findIndex((item) => item.Nome.toUpperCase() === nome.toUpperCase());
+    indiceItemHortiFruti = EstoqueHortiFruti.findIndex((item) => item.Nome.toUpperCase() === nome.toUpperCase());
+    
+    if (indiceItemMercearia === -1 && indiceItemHortiFruti === -1) {
+      console.log('Item inválido, retornaremos ao menu principal: ')
+    }
+
+
+    if (indiceItemMercearia > -1) {
+      console.log(`\n ${EstoqueMercearia[indiceItemMercearia].Nome} foi selecionado`);
+      
+      quantidade = input.question(`Insira a quantidade de ${EstoqueMercearia[indiceItemMercearia].Nome} que deseja comprar: `);
+      
+      while (quantidade > EstoqueMercearia[indiceItemMercearia].Quantidade) {
+        quantidade = input.question('Não há itens o suficiente, insira uma quantidade válida: ');
+      }
+
+      EstoqueMercearia[indiceItemMercearia].Quantidade -= quantidade;
+
+
+      if (EstoqueMercearia[indiceItemMercearia].Quantidade === 0) {
+        EstoqueMercearia.splice(indiceItemMercearia, 1);
+      }
+    }
+    if (indiceItemHortiFruti > -1) {
+      console.log(`\n ${EstoqueHortiFruti[indiceItemHortiFruti].Nome} foi selecionado`);
+      quantidade = input.question(`Insira a quantidade de ${EstoqueHortiFruti[indiceItemHortiFruti].Nome} que deseja comprar: `);
+      
+      while (quantidade > EstoqueHortiFruti[indiceItemHortiFruti].Quantidade) {
+        quantidade = input.question('Não há itens o suficiente, insira uma quantidade válida: ');
+      }
+
+      EstoqueHortiFruti[indiceItemHortiFruti].Quantidade -= quantidade;
+
+
+      if (EstoqueHortiFruti[indiceItemHortiFruti].Quantidade === 0) {
+        EstoqueHortiFruti.splice(indiceItemHortiFruti, 1);
+      }
+    }
+
+    exportarEstoque({EstoqueMercearia: EstoqueMercearia, EstoqueHortiFruti: EstoqueHortiFruti});
+
+    console.log('Compra finalizada, volte sempre!');
+  } else if (opcao == 5) {
+    mostrarItensEstoque();
   }
 }
 
+console.log('\n');
 console.log('Sistema finalizado!');
